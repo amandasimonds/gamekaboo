1534cb0b80d2de5cd5a60a611efd398ff35549bc
Amanda Tree <amandasimonds9@gmail.com>
2020-07-15 14:32:08 -0600

    tweaked the styling and images. still have some learning to do

--- src/components/CommunityBox.js
+++ src/components/CommunityBox.js
@@ -1,21 +1,20 @@
 import React from "react";
 import "../styles/main.scss"
-import CommunityImg from "../img/aidan-howe.jpg"
+import CommunityImg from "../img/manny-moreno.jpg"
 
 const communityBox = (props) => (
+    <div>
     <div className="section-community">
+                <span className="heading-2">Join our community and contribute your own content!
+                </span> 
+                
+                <p class="wrap-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Nunc aliquet bibendum enim facilisis gravida neque convallis a cras. Ut pharetra sit amet aliquam. Pharetra et ultrices neque ornare aenean. Velit sed ullamcorper morbi tincidunt. Ipsum dolor sit amet consectetur adipiscing. Fermentum iaculis eu non diam phasellus vestibulum. Nibh praesent tristique magna sit amet purus. At tellus at urna condimentum mattis pellentesque id nibh tortor. Enim facilisis gravida neque convallis. Faucibus turpis in eu mi bibendum neque egestas congue. Sem viverra aliquet eget sit amet tellus. Nisl rhoncus mattis rhoncus urna neque. Massa tincidunt dui ut ornare. Leo a diam sollicitudin tempor. Faucibus et molestie ac feugiat sed lectus. Viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est. Consequat id porta nibh venenatis cras sed felis.
 
-        <div className="row">
-            <div className="col-1-of-2">
-                <span className="heading-2">Join our community and contribute your own content!</span>
-            </div>
-
-            <div className="col-1-of-2">
-                <img className="section-community--img" src={CommunityImg} alt="Community Img"></img>
-            </div>
-        </div>
-
+Phasellus vestibulum lorem sed risus ultricies. Augue lacus viverra vitae congue eu consequat ac felis. Dictum fusce ut placerat orci nulla. Netus et malesuada fames ac turpis. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Feugiat vivamus at augue eget arcu dictum varius duis. Sit amet mattis vulputate enim nulla aliquet porttitor lacus. Dictum non consectetur a erat nam at lectus urna duis. Vel facilisis volutpat est velit egestas dui id ornare arcu. Dignissim convallis aenean et tortor at risus viverra. Turpis massa tincidunt dui ut ornare lectus sit amet est. Volutpat odio facilisis mauris sit amet. Nulla porttitor massa id neque aliquam vestibulum morbi blandit. Integer eget aliquet nibh praesent tristique magna sit amet. Eget velit aliquet sagittis id consectetur purus ut faucibus. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Purus in massa tempor nec feugiat. Commodo viverra maecenas accumsan lacus.</p>
     </div>
+    <img className="section-community--img" src={CommunityImg} alt="Community Img"></img>
+    </div>
+    
 )
 
 export default communityBox
